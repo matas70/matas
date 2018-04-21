201ba5c91c1b17efc498f247f8f3c1c623d9a991
Lior Tamam <leo212@gmail.com>
2018-04-21 14:31:08 +0300

    fixed serviceworker to make chrome suggest "add site to home screen"

--- js/functions.js
+++ js/functions.js
@@ -495,16 +495,14 @@ function onAboutButtonClick() {
 
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
-            navigator.serviceWorker.register('/js/service-worker.js').then(function (registration) {
+            navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }, function (err) {
                 // registration failed :(
                 console.log('ServiceWorker registration failed: ', err);
             });
-        });
-    }
+        }
 }
 
 var currentLocationMarker;
