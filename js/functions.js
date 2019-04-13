5b579a8ee0759bdc0fcb9d0aab14dc031b2c7466
nitzanmo <nitzanmousan@gmail.com>
2019-04-13 23:55:16 +0300

    Merged with master properly

--- js/functions.js
+++ js/functions.js
@@ -962,16 +962,16 @@ function selectPoint(pointId, minimized = false) {
         deselectLocation(function () {
             // then show a new popup
             selectLocation(pointId, convertLocation(selectedPoint.N, selectedPoint.E), marker,
-                mapAPI.getMarkerIcon(selectedRoute.color, false, isPointAerobatic(pointId)),
-                mapAPI.getMarkerIcon(selectedRoute.color, true, isPointAerobatic(pointId)),
+                mapAPI.getMarkerIcon(selectedRoute.color, false, isPointAerobatic(pointId), selectedPoint.pointName),
+                mapAPI.getMarkerIcon(selectedRoute.color, true, isPointAerobatic(pointId), selectedPoint.pointName),
                 "#" + selectedRoute.color, "#" + selectedRoute.primaryTextColor,
                 "#" + selectedRoute.secondaryTextColor, minimized);
         });
     } else {
         // then show a new popup
         selectLocation(pointId, convertLocation(selectedPoint.N, selectedPoint.E), marker,
-            mapAPI.getMarkerIcon(selectedRoute.color, false, isPointAerobatic(pointId)),
-            mapAPI.getMarkerIcon(selectedRoute.color, true, isPointAerobatic(pointId)),
+            mapAPI.getMarkerIcon(selectedRoute.color, false, isPointAerobatic(pointId), selectedPoint.pointName),
+            mapAPI.getMarkerIcon(selectedRoute.color, true, isPointAerobatic(pointId), selectedPoint.pointName),
             "#" + selectedRoute.color, "#" + selectedRoute.primaryTextColor,
             "#" + selectedRoute.secondaryTextColor, minimized);
     }
