7f5f1b8f8907947993b5b8158af1b915e7361af3
Lior Tamam <leo212@gmail.com>
2019-04-23 16:20:22 +0300

    fix cache of serviceworker itself.

--- js/functions.js
+++ js/functions.js
@@ -538,7 +538,7 @@ function onAboutButtonClick() {
 
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?t='+(new Date()).getTime()).then(function (registration) {
+            navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }, function (err) {
