ca109091b73623802485f4b48d8f754bb9090615
Dani N <nvdvrts@gmail.com>
2020-04-03 19:15:34 +0300

    Add relative server env stage checks and set endpoints accordingly

--- js/functions.js
+++ js/functions.js
@@ -37,6 +37,29 @@ var userSimulation = false;
 var aircraftData = null;
 var appLoaded = false;
 var changes = false;
+var appStage;
+var apiURL = "https://matasstorage.blob.core.windows.net"
+
+function getEnv(callback) {
+    if (appStage != undefined) callback(appStage);
+    else {
+        $.getJSON('data/env.json', (body) => {
+            switch (body.env) {
+                case 'dev':
+                    appStage = 'matas-dev';
+                    break;
+
+                case 'prod':
+                    appStage = 'matas';
+                    break;
+
+                default:
+                    break;
+            }
+            callback(appStage);
+        });
+    }
+}
 
 function convertLocation(north, east) {
     var latDegrees = Math.floor(north / 100);
@@ -471,12 +494,14 @@ function updateLocations(route) {
 }
 
 function loadRoutes(callback) {
-    $.getJSON("data/routes.json?t="+(new Date()).getTime(), function (routes) {
-        routes.routes.forEach(function (route) {
-            updateLocations(route);
-        }, this);
-        loadedRoutes = routes.routes;
-        callback(routes.routes);
+    getEnv((env) => {
+        $.getJSON(`${apiURL}/${env}/routes.json?t=`+(new Date()).getTime(), function (routes) {
+            routes.routes.forEach(function (route) {
+                updateLocations(route);
+            }, this);
+            loadedRoutes = routes.routes;
+            callback(routes.routes);
+        });
     });
 }
 
@@ -518,55 +543,57 @@ function loadActualStartTime() {
 }
 
 function loadAircrafts(callback) {
-    $.getJSON("data/aircrafts-info.json?t="+(new Date()).getTime(), function(aircraftInfo) {
-        // load aircraft type info into a map
-        aircraftInfo.aircraftTypes.forEach(function (aircraftTypeInfo) {
-            aircraftTypesInfo[aircraftTypeInfo.aircraftTypeId] = aircraftTypeInfo;
-        }, this);
-
-        // load all aircrafts
-        $.getJSON("data/aircrafts.json?t="+(new Date()).getTime(), function (flightData) {
-            aircrafts = flightData.aircrafts;
-            startDate = flightData.startDate;
-            plannedStartTime = convertTime(startDate, flightData.plannedStartTime);
-            plannedEndTime = convertTime(startDate, flightData.plannedEndTime);
-            changes = flightData.changes;
-
-            // merge info from aircraft type info
-            aircrafts.forEach(function (aircraft) {
-                if (aircraft.aircraftTypeId !== undefined) {
-                    // copy all of the information from aircraft type info
-                    var aircraftTypeInfo = aircraftTypesInfo[aircraft.aircraftTypeId];
-                    for(var field in aircraftTypeInfo)
-                        aircraft[field]=aircraftTypeInfo[field];
-                }
-
-                // sort aircraft path by time
-                aircraft.path.sort((point1, point2) => convertTime(point1.date, point1.time) - convertTime(point2.date, point2.time));
-
-                // update times of all flights
-                if (!aircraft.hide && aircraft.path.length > 0 && !aircraft.special) {
-                    aircraftFlightTime = convertTime(aircraft.path[0].date, aircraft.path[0].time);
-                    if (firstFlightTime == null) {
-                        firstFlightTime = aircraftFlightTime;
-                    } else if (aircraftFlightTime < firstFlightTime) {
-                        firstFlightTime = aircraftFlightTime;
-                    }
-
-                    aircraftLandTime = convertTime(aircraft.path[aircraft.path.length-1].date, aircraft.path[aircraft.path.length-1].time);
-
-                    if (lastFlightTime == null) {
-                        lastFlightTime = aircraftLandTime;
-                    } else if (aircraftLandTime > lastFlightTime) {
-                        lastFlightTime = aircraftLandTime;
-                    }
-                }
-
+    getEnv((env) => {
+        $.getJSON(`${apiURL}/${env}/aircrafts-info.json?t=` + (new Date()).getTime(), function(aircraftInfo) {
+            // load aircraft type info into a map
+            aircraftInfo.aircraftTypes.forEach(function (aircraftTypeInfo) {
+                aircraftTypesInfo[aircraftTypeInfo.aircraftTypeId] = aircraftTypeInfo;
             }, this);
 
-            aircraftData = flightData;
-            loadActualStartTime();
-            callback(aircrafts);
+            // load all aircrafts
+            $.getJSON(`${apiURL}/${env}/aircrafts.json?t=`+(new Date()).getTime(), function (flightData) {
+                aircrafts = flightData.aircrafts;
+                startDate = flightData.startDate;
+                plannedStartTime = convertTime(startDate, flightData.plannedStartTime);
+                plannedEndTime = convertTime(startDate, flightData.plannedEndTime);
+                changes = flightData.changes;
+
+                // merge info from aircraft type info
+                aircrafts.forEach(function (aircraft) {
+                    if (aircraft.aircraftTypeId !== undefined) {
+                        // copy all of the information from aircraft type info
+                        var aircraftTypeInfo = aircraftTypesInfo[aircraft.aircraftTypeId];
+                        for(var field in aircraftTypeInfo)
+                            aircraft[field]=aircraftTypeInfo[field];
+                    }
+
+                    // sort aircraft path by time
+                    aircraft.path.sort((point1, point2) => convertTime(point1.date, point1.time) - convertTime(point2.date, point2.time));
+
+                    // update times of all flights
+                    if (!aircraft.hide && aircraft.path.length > 0 && !aircraft.special) {
+                        aircraftFlightTime = convertTime(aircraft.path[0].date, aircraft.path[0].time);
+                        if (firstFlightTime == null) {
+                            firstFlightTime = aircraftFlightTime;
+                        } else if (aircraftFlightTime < firstFlightTime) {
+                            firstFlightTime = aircraftFlightTime;
+                        }
+
+                        aircraftLandTime = convertTime(aircraft.path[aircraft.path.length-1].date, aircraft.path[aircraft.path.length-1].time);
+
+                        if (lastFlightTime == null) {
+                            lastFlightTime = aircraftLandTime;
+                        } else if (aircraftLandTime > lastFlightTime) {
+                            lastFlightTime = aircraftLandTime;
+                        }
+                    }
+
+                }, this);
+
+                aircraftData = flightData;
+                loadActualStartTime();
+                callback(aircrafts);
+            });
         });
     });
 }
