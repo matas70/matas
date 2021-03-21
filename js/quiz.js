f0609d8ec13b23b0508b5f1514f43de0c8a03601
Noam G <noam.gaash@gmail.com>
2021-03-21 15:29:29 +0200

    feat: add first question

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,20 @@
 (function () {
 
+    var questions = [
+        {
+            title: 'איזה מטוס הוא הכי מהיר מבין השלושה?',
+            a: 'עפרוני',
+            b: 'אדיר',
+            c: 'עפיפון',
+            correct: 'b',
+            category: 'מטוסי קרב'
+        }
+    ];
+
+    var count = 0,
+        maxQuestions = 5;
+
+
     // create the quiz button
     var enterQuizButton = document.createElement("div");
     enterQuizButton.classList.add('button', 'on-map', 'quiz');
@@ -11,6 +26,20 @@
         $('#quiz').css('display', 'block').animate({
             'opacity': 1
         }, 1000)
-    })
 
+        questions = questions.sort(()=>Math.random()-.5);
+
+        showNextQuestion();
+
+    });
+
+    function showNextQuestion() {
+        var question = questions[count++];
+        $('#quiz .title').text(question.title)
+        $('#quiz .option.a').text(question.a)
+        $('#quiz .option.b').text(question.b)
+        $('#quiz .option.c').text(question.c)
+        $('#quiz .counter .current').text(count)
+        $('#quiz .counter .total').text(maxQuestions)
+    }
 })()
