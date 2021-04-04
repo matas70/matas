d871b7672d119c5ced5b544de92ecf8fd8e05441
idan0540 <idan0540@gmail.com>
2021-04-04 12:05:14 +0300

    style: design 'start ar' button

--- ARButton.js
+++ ARButton.js
@@ -80,10 +80,6 @@ class ARButton {
 
 			button.style.display = '';
 
-			button.style.cursor = 'pointer';
-			button.style.left = 'calc(50% - 50px)';
-			button.style.width = '100px';
-
 			button.textContent = 'תלת מימד';
 
 			button.onmouseenter = function () {
@@ -152,18 +148,6 @@ class ARButton {
 
 		function stylizeElement( element ) {
 
-			element.style.position = 'absolute';
-			element.style.bottom = '20px';
-			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
-			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
-			element.style.color = '#fff';
-			element.style.font = 'normal 13px sans-serif';
-			element.style.textAlign = 'center';
-			element.style.opacity = '0.5';
-			element.style.outline = 'none';
-			element.style.zIndex = '999';
 
 		}
 
