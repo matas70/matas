9ecd3049dc473a64886c04e0b23f6105cfc04a25
may peleg <maypeleg00@gmail.com>
2022-04-29 17:02:53 +0300

    fix bug with 'המתנות מוזיאון'

--- js/functions.js
+++ js/functions.js
@@ -1693,7 +1693,7 @@ function displaySearchView() {
         sortedLocations.forEach(function (location) {
             if (((!location.hidden &&
                 !!routes.find(route => route.points.find(point => location.pointId === point.pointId)) )|| isNotHidden(location)) &&
-                (location.type !== "base" || (!location.pointName.includes('בסיס')&& location.pointName !== 'בסיס חצור') || !location.pointName.includes('מוזיאון חיל האוויר') || location.type !== "hospital" || !location.pointName.includes('תצפית למטס'))) {
+                (location.type !== "base" || (!location.pointName.includes('בסיס')&& location.pointName !== 'בסיס חצור') || !location.pointName.includes('מוזיאון ') || location.type !== "hospital" || !location.pointName.includes('תצפית למטס'))) {
                     searchViewHtml += createLocationRow(location, false, true);
             }
         }, this);
@@ -2182,7 +2182,7 @@ function fillMenu() {
     sortedLocations.forEach(function (location) {
         if (((!location.hidden &&
             !!routes.find(route => route.points.find(point => location.pointId === point.pointId))) || isNotHidden(location)) &&
-            ( (!location.pointName.includes('בסיס') || location.pointName === 'בסיס חצור') && !location.pointName.includes('מוזיאון חיל האוויר') && location.type !== "hospital" && !location.pointName.includes('תצפית למטס'))) {
+            ( (!location.pointName.includes('בסיס') || location.pointName === 'בסיס חצור') && !location.pointName.includes('מוזיאון') && location.type !== "hospital" && !location.pointName.includes('תצפית למטס'))) {
                 locationsViewHtml += createLocationRow(location, false);
         }
     }, this);
