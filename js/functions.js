d85c17a2ecccea54bfcbe4b95a63387235f329dd
may peleg <maypeleg00@gmail.com>
2022-05-04 20:39:04 +0300

    add weather popup

--- js/functions.js
+++ js/functions.js
@@ -2641,9 +2641,9 @@ function haversineDistance(coords1, coords2) {
 
 if (Cookies.get('seen_welcome_2021') != '1') {
     
-    // $('.new-popup').show()
+     $('.new-popup').show()
     $('.new-popup button, .new-popup .quiz, .new-popup .ar').on('click', () => {
-        // $('.new-popup').fadeOut(200);
+         $('.new-popup').fadeOut(200);
         Cookies.set('seen_welcome_2021', '1', { expires: 10000});
     })
     $('.new-popup .ar').on('click', ()=>openAR());
