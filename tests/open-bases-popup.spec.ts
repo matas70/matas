8c49bb0a6e0908e7db8026518dc8ffe0bf892731
Noam Gaash <noam.gaash@gmail.com>
2022-10-31 08:51:40 +0200

    test: add beforetest navigation

--- tests/open-bases-popup.spec.ts
+++ tests/open-bases-popup.spec.ts
@@ -1,23 +1,28 @@
 import test, { expect } from "@playwright/test";
 
+test.describe("Open Bases Popup", () => {
 
-test('popup opens', async ({ page }) => {
-    await page.goto('https://127.0.0.1:8080/#main');
-    const popup = page.locator('.open-bases-popup');
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://127.0.0.1:8080/#main");
+    });
 
-    await expect(popup).not.toBeVisible();
-    await page.click('.base-category-container');
-    await expect(popup).toBeVisible();
+    test('popup opens', async ({ page }) => {
+        const popup = page.locator('.open-bases-popup');
 
-});
+        await expect(popup).not.toBeVisible();
+        await page.click('.base-category-container');
+        await expect(popup).toBeVisible();
 
-test('baseName has hebrew text', async ({ page }) => {
-    await page.goto('https://127.0.0.1:8080/#main');
-    const popup = page.locator('.open-bases-popup');
-    const baseName = page.locator('#baseName');
+    });
 
-    await page.click('.base-category-container');
-    await expect(popup).toBeVisible();
-    await expect(baseName).toBeVisible();
-    await expect(baseName).toHaveText(/[\u0590-\u05FF \w]/);
+    test('baseName has hebrew text', async ({ page }) => {
+        const popup = page.locator('.open-bases-popup');
+        const baseName = page.locator('#baseName');
+
+        await page.click('.base-category-container');
+        await expect(popup).toBeVisible();
+        await expect(baseName).toBeVisible();
+        await expect(baseName).toHaveText(/[\u0590-\u05FF \w]/);
+    });
+
 });
