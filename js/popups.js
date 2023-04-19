56681638e8f9c61e3030c023bf0ea2f9c591c174
ayeletLus <ayeletlus3255@gmail.com>
2023-04-19 14:41:24 +0300

    added correct video url and design

--- js/popups.js
+++ js/popups.js
@@ -728,6 +728,37 @@ function showAircraftInfoPopup(aircraft, collapse) {
   $("#aircraftInfoName").text(aircraft.name);
   $("#aircraftInfoType").text(aircraft.type);
 
+
+  const aircraftsVideoId = [
+    {name: "אדיר", id: "0pM6bGZWGRU"},
+    {name: "איתן", id: "XCMu7WQS96c"},
+    {name: "בז", id: "zlFwy_qrMM8"},
+    {name: "ברק", id: "pcQ1KSKmDis"},
+    {name: "הרווארד", id: "BjOCrBv1zZk"},
+    {name: "ינשוף", id: "ivj5smYeJGY"},
+    {name: "יסעור", id: "s-n8DhWX3PE"},
+    {name: "לביא", id: "xdAFBKKWMKo"},
+    {name: "נחשון", id: "K5K5KaloSfg"},
+    {name: "סופה", id: "W7zWeE0kchc"},
+    {name: "סנונית", id: "NuQzvdGowOE"},
+    {name: "עטלף", id: "3vzUo9JAygU"},
+    {name: "עפרוני", id: "Kfkd5GEymvA"},
+    {name: "פתן", id: "PK0FqDks8_Q"},
+    {name: "צופית", id: "lW7tx5lcCTE"},
+    {name: "קרנף", id: "hkJ0u40mKCs"},
+    {name: "ראם", id: "J2N_PReaobE"},
+    {name: "רעם", id: "SuxqbK3PjC0"},
+    {name: "שובל", id: "Sv3WdFLnlCw"},
+    {name: "שמשון", id: "_3_4btQCiS4"},
+    {name: "שרף", id: "77mbzmecJdc"},
+];
+    //find matching youtube video
+    let airplaneVideo = document.querySelector('#aircraftInfoVideo');
+    let airplaneVideoName = document.querySelector('#aircraftInfoName').innerText;
+    const currAircraft = aircraftsVideoId.find(aircraft =>  aircraft.name === airplaneVideoName);
+    airplaneVideo.src = `https://www.youtube.com/embed/${currAircraft.id}`;
+
+
   if (
     (aircraft.name === "צופית") |
     (aircraft.name === "עפרוני") |
